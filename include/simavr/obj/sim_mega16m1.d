31a7df4a773cc019eb792115337fa23e583b00dd
santigoro@gmail.com <santigoro@gmail.com@8e8f3913-bc4d-05c7-239d-b92c8785a94d>
2012-05-23 14:56:35 +0000

    Cleaning

--- include/simavr/obj/sim_mega16m1.d
+++
@@ -1,8 +0,0 @@
-obj/sim_mega16m1.o: cores/sim_mega16m1.c sim/sim_avr.h sim/sim_irq.h \
- /usr/lib/avr/include/stdint.h sim/sim_interrupts.h sim/sim_avr_types.h \
- /usr/lib/avr/include/inttypes.h sim/sim_cycle_timers.h sim/sim_io.h \
- sim/sim_avr.h sim/sim_regbit.h /usr/lib/avr/include/avr/iom16m1.h \
- cores/sim_megaxm1.h cores/sim_core_declare.h sim/avr_eeprom.h \
- sim/avr_flash.h sim/avr_watchdog.h sim/avr_extint.h sim/avr_ioport.h \
- sim/avr_lin.h sim/avr_uart.h sim/fifo_declare.h sim/avr_adc.h \
- sim/avr_timer.h sim/avr_spi.h
--- include/simavr/obj/sim_mega644.d
+++
@@ -1,9 +0,0 @@
-obj/sim_mega644.o: cores/sim_mega644.c sim/sim_avr.h sim/sim_irq.h \
- /usr/lib/avr/include/stdint.h sim/sim_interrupts.h sim/sim_avr_types.h \
- /usr/lib/avr/include/inttypes.h sim/sim_cycle_timers.h sim/sim_io.h \
- sim/sim_avr.h sim/sim_regbit.h /usr/lib/avr/include/avr/iom644.h \
- /usr/lib/avr/include/avr/iomxx4.h cores/sim_megax4.h \
- cores/sim_core_declare.h sim/avr_eeprom.h sim/avr_flash.h \
- sim/avr_watchdog.h sim/avr_extint.h sim/avr_ioport.h sim/avr_uart.h \
- sim/fifo_declare.h sim/avr_adc.h sim/avr_timer.h sim/avr_spi.h \
- sim/avr_twi.h
