be8dbe9bda941db8dd868722c816a3ab08575779
santigoro@gmail.com <santigoro@gmail.com@8e8f3913-bc4d-05c7-239d-b92c8785a94d>
2012-05-23 04:18:41 +0000

    Cleaning

--- include/simavr-0/obj/sim_mega328.d
+++
@@ -1,8 +0,0 @@
-obj/sim_mega328.o: cores/sim_mega328.c sim/sim_avr.h sim/sim_irq.h \
- /usr/lib/avr/include/stdint.h sim/sim_interrupts.h sim/sim_avr_types.h \
- /usr/lib/avr/include/inttypes.h sim/sim_cycle_timers.h sim/sim_io.h \
- sim/sim_avr.h sim/sim_regbit.h /usr/lib/avr/include/avr/iom328p.h \
- cores/sim_megax8.h cores/sim_core_declare.h sim/avr_eeprom.h \
- sim/avr_flash.h sim/avr_watchdog.h sim/avr_extint.h sim/avr_ioport.h \
- sim/avr_uart.h sim/fifo_declare.h sim/avr_adc.h sim/avr_timer.h \
- sim/avr_spi.h sim/avr_twi.h
--- include/simavr-0/obj/sim_tinyx4.d
+++
@@ -1,9 +0,0 @@
-obj/sim_tinyx4.o: cores/sim_tinyx4.c /usr/lib/avr/include/stdio.h \
- /usr/lib/avr/include/inttypes.h /usr/lib/avr/include/stdint.h \
- /usr/lib/gcc/i486-linux-gnu/4.4.3/include/stdarg.h \
- /usr/lib/gcc/i486-linux-gnu/4.4.3/include/stddef.h sim/sim_avr.h \
- sim/sim_irq.h sim/sim_interrupts.h sim/sim_avr_types.h \
- sim/sim_cycle_timers.h sim/sim_io.h sim/sim_avr.h sim/sim_regbit.h \
- cores/sim_tinyx4.h cores/sim_core_declare.h sim/avr_eeprom.h \
- sim/avr_watchdog.h sim/avr_extint.h sim/avr_ioport.h sim/avr_adc.h \
- sim/avr_timer.h
